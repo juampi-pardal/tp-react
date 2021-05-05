5849817d913c3aa8d4dbfc127e035d3633d0dac1
Juan pablo pardal <pardaljpp@gmail.com>
2021-05-05 13:16:17 -0300

    remove dist

--- dist/routes/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiRoutes = void 0;
-const detail_routes_1 = require("./detail-routes");
-const search_routes_1 = require("./search-routes");
-exports.apiRoutes = [search_routes_1.SearchRoutes, detail_routes_1.DetailRoutes];
